b5bb6615c6567a3632e3ae94552e56c3a28d7e50
faturrumahan <faturrumahan@gmail.com>
2024-08-23 16:57:48 +0700

    cors

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  const corsOptions: CorsOptions = {
+    origin: 'https://example.com', // Specify allowed origin(s)
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Specify allowed methods
+    credentials: true, // Allow credentials (cookies, authorization headers, etc.)
+    allowedHeaders: 'Content-Type, Authorization', // Specify allowed headers
+  };
+  app.enableCors(corsOptions);
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
